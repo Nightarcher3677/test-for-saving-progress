c8a99fe311eaae013aacebb0284c7d8add906999
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-24 10:44:28 +1100

    began work on stage 2

---
+++ stage2.py
@@ -0,0 +1,12 @@
+
+
+print('you see a codepad in front of you. next to it is a sign that says BRIDGE')
+while 1 = 1:
+    choice = input('above both of those signs, there is a screen that displays the words: CAPTAIN ANAGRAM \n it then flickers to read "lttree ot bunmre" then it flickers to ERROR 16 1 19 19 23 15 18 4  \n\nCode: ')
+    if choice == 'password':
+        print("as you enter the password (while making a mental not of the previous captain's lack of imagination), \n the screen flickers once more to reveal the words WELCOME CAPTAIN GANRAAM.")
+        break
+    else:
+        print('The screen flickers and displays: INCORRECT PASSWORD')
+
+print("you walk into the bridge, and see the control panel. it's still running, as if the occupants had suddenly disappeared.")

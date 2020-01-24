474a6c6eba937eb45c0414734e47fc95ee67b016
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-24 13:14:14 +1100

    finished stage 2

--- stage2.py
+++ stage2.py
@@ -1,6 +1,7 @@
 import time
 import os
-
+import random
+x = 1
 print('you see a codepad in front of you. next to it is a sign that says BRIDGE')
 while 1 == 1:
     choice = input('above both of those signs, there is a screen that displays the words: CAPTAIN ANAGRAM \n it then flickers to read "lttree ot bunmre" then it flickers to ERROR 16 1 19 19 23 15 18 4  \n\nCode: ')
@@ -16,6 +17,7 @@ while 1 == 1:
     choice = input('you can: look around, inpect control panel, poke chair \n\n')
     if choice == 'poke chair':
         print("wow, that's really soft")
+        chair = 1
     elif choice == 'look around':
         print('not much else to see')
     elif choice == 'inspect control panel':
@@ -25,20 +27,76 @@ while 1 == 1:
         print('that is not an action')
 print('you jam your finger on the home button that is displayed on the lower right corner, and activate the shields program. the ship approaches you.')
 print('(this can cause the story to diverge) the ship approaching you attempts to make contact.')
-choice = input('do you: accept the call [y], deny the call [n]')
 while 1 == 1:
     choice = input('do you: accept the call [y], deny the call [n]')
-    if choice == 'poke chair':
+    if choice == 'y':
         print("you have accepted the call")
-        s = open('stage.txt', 'w')
-        s.write('branch1')
-        os.popen('branch1.py')
+        accepted = True
         break
-    elif choice == 'inspect control panel':
+    elif choice == 'n':
         print('you have denied the call')
-        s = open('stage.txt', 'w')
-        s.write('branch2')
-        os.popen('branch2.py')
+        accepted = False
         break
     else:
         print('that is not an action')
+if accepted == True:
+    print('A holographic screen appears before you, showing the face of a man who appeared to be in his early thirties \n, his left eye replaced with a shiny black lens that must have been the end of a camera')
+else:
+    print('the ship, now in range of your vision, fires on you. Your shields are at 96%')
+
+    while x == 1:
+        sh = 96
+        choice = input('do you: fire back, run away')
+        if choice == 'fire back':
+            print("you fire back. the blast brings their shields down to 98%")
+            sh = 96
+            eh = 98
+            p = 1
+            while p == 1:
+                if eh > 0:
+                    choice = input('you can: fire, run')
+                    if choice == 'fire':
+                        eh = eh - random.randint(1,6)
+                        print("the enemy ship's shields are at " + str(eh) + '%')
+                        sh = sh - random.randint(1,5)
+                        print("your ship's shields are at " + str(sh) + '%')
+                    elif choice == 'run':
+                        run = random.randint(1,15)
+                        if run == 1:
+                            print('Your ship blasts in the opposite direction to the enemy ship, which is crushed \n by a passing asteroid (how convenient)')
+                            break
+                        else:
+                            print('the enemy ship follows you, and blasts you before you can get away')
+                            sh = sh - random.randint(1,5)
+                            print("your ship's shields are at " + str(sh) + '%')
+                elif sh < 0:
+                    print('you died. better luck next time! (just kidding! you suck at this)')
+                    break
+                else:
+                    print('the enemy ship is destroyed by your final blast, and the debris is scattered throughout space')
+                    x = 2
+                    break
+        elif choice == 'run away':
+            run = random.randint(1,15)
+            if run == 1:
+                print('Your ship blasts in the opposite direction to the enemy ship, which is crushed \n by a passing asteroid (how convenient)')
+                x = 2
+                break
+            else:
+                print('the enemy ship follows you, and blasts you before you can get away')
+                sh = sh - random.randint(1,5)
+                print("your ship's shields are at " + str(sh) + '%')
+                choice = 'fire back'
+        else:
+            print('that is not an action')
+if chair == 1:
+    print('with the enemy ship gone, you can finally relax. you poke the chair, as if it would have somehow become hard. it is still soft')
+else:
+    print('with the enemy ship gone, you can finally relax.')
+
+s = open('stage.txt', 'w')
+s.write('stage3')
+s = open('stage.txt', 'r')
+read = s.read()
+hg = input('you can now safely press the X button without losing your progress \n\n')
+os.popen('stage3.py')
--- stagepick.py
+++ stagepick.py
@@ -4,10 +4,15 @@ s = open('stage.txt', 'a')
 s = open('stage.txt', 'r')
 j = s.read()
 print('your current stage is ' + j)
-
-stage = input('pick your stage (1, 2, branch1, branch2) \n\n')
-
-if stage == '1':
-    os.popen('stage1.py')
-elif stage == '2':
-    os.popen('stage2.py')
+q = 1
+stage = input('pick your stage (1, 2, 3) \n\n')
+while q == 1:
+    if stage == '1':
+        os.popen('stage1.py')
+        break
+    elif stage == '2':
+        os.popen('stage2.py')
+        break
+    elif stage == '3':
+        os.popen('stage3.py')
+        break
